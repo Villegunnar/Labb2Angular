2542904dfbd30cf3e8175c6750ea310bb09453f2
Viktor Gunnarsson <villegunnar@gmail.com>
2022-10-02 21:17:02 +0200

    testing Commit Labb2 Angular

---
+++ src/app/Models/employee.model.ts
@@ -0,0 +1,13 @@
+export interface Employee{
+    id:string;
+    FirstName:string;
+    LastName:string;
+    Email:string;
+    PhoneNumber:string;
+    Gender:string;
+
+    DepartmentId:string;
+    Adress:string;
+    Salary:string;
+
+}
---
+++ src/app/service/employee.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from "@angular/common/http";
+import { Employee } from "../Models/employee.model";
+import {Injectable} from '@angular/core'
+import {Observable} from 'rxjs'
+
+
+@Injectable({
+    providedIn : 'root'
+});
+export class EmployeeService{
+    baseUrl='https://localhost:7158/api/employee'
+
+    constructor(private http:HttpClient) { }
+
+
+
+// Get Employee
+getAllEmployees():Observable<Employee[]>
+{
+    return this.http.get<Employee[]>(this.baseUrl);
+}
+
+
+
+// Add new Employee
+
+addEmployee(employee:Employee):Observable<Employee>
+{
+    employee.id = '00000000-0000-0000-0000-000000000000';
+
+    return this.http.post<Employee>(this.baseUrl,employee)
+}
+
+
+
+// Delete Employee
+deleteEmployee(id:string):Observable<Employee>
+{
+    return this.http.delete<Employee>(this.baseUrl + '/' + id)
+}
+
+//Update Employee
+updateEmployee(employee:Employee):Observable<Employee>
+{
+    return this.http.put<Employee>(this.baseUrl + '/' + employee.id,employee)
+}
+
+
+
+}
+
